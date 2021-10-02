743fd7841ec6283b63a438d9c705f0051f110de8
Vizdun <viz@vizarch.localdomain>
2021-10-02 14:00:29 +0200

    question count on instructions page

--- src/pages/instructions.ts
+++ src/pages/instructions.ts
@@ -1,10 +1,10 @@
-import { general } from "../data"
+import { general, questions } from "../data"
 
 export const instructions = `<h1>${general.title}</h1>
 <hr />
 <h2 style="text-align: center">Instructions</h2>
 <p class="question">
-	You will be presented with a series of statements. For each one, click the
+	You will be presented with ${questions.length} statements. For each one, click the
 	button with your opinion on it.
 </p>
 <button class="button" id="quizButton">Got it!</button>
