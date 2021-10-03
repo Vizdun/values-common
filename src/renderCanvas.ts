3b39d078e200f3565931a80d476c60084444507d
Vizdun <viz@vizarch.localdomain>
2021-10-03 19:39:09 +0200

    actual fix for the canvas issue

--- src/renderCanvas.ts
+++ src/renderCanvas.ts
@@ -10,9 +10,6 @@ export function renderCanvas(
 	ctx.fillStyle = canvas.bgColor
 	ctx.fillRect(0, 0, 800, 170 + 120 * axes.length)
 
-	var img: HTMLImageElement
-	img = new Image(200, 200)
-
 	ctx.fillStyle = canvas.fgColor
 
 	ctx.font =
@@ -35,51 +32,56 @@ export function renderCanvas(
 	ctx.fillText(general.link, 780, 60)
 	ctx.fillText(general.version, 780, 90)
 
-	for (let i = 0; i < 2; i++) {
-		axes.forEach((axis, index) => {
-			const height = 120 * index
-			const iHeight = 170 + height
-			const bHeight = 180 + height
-			const biHeight = 184 + height
-			const bitHeight = 237.5 + height
-			const biTHeight = 175 + height
+	axes.forEach((axis, index) => {
+		const height = 120 * index
+		const iHeight = 170 + height
+		const bHeight = 180 + height
+		const biHeight = 184 + height
+		const bitHeight = 237.5 + height
+		const biTHeight = 175 + height
 
-			img.src = fallbackImage(axis, false)
+		var img = new Image(200, 200)
+		img.onload = () => {
 			ctx.drawImage(img, 20, iHeight, 100, 100)
-			img.src = fallbackImage(axis, true)
-			ctx.drawImage(img, 680, iHeight, 100, 100)
+		}
+		img.src = fallbackImage(axis, false)
 
-			ctx.fillStyle = canvas.fgColor
-			ctx.fillRect(120, bHeight, 560, 80)
+		var img2 = new Image(200, 200)
+		img2.onload = () => {
+			ctx.drawImage(img2, 680, iHeight, 100, 100)
+		}
+		img2.src = fallbackImage(axis, true)
 
-			ctx.fillStyle = axis.left.color
-			ctx.fillRect(120, biHeight, 5.6 * resultEffects[axis.id] - 2, 72)
-			ctx.fillStyle = axis.right.color
-			ctx.fillRect(
-				682 - 5.6 * (100 - resultEffects[axis.id]),
-				biHeight,
-				5.6 * (100 - resultEffects[axis.id]) - 2,
-				72
+		ctx.fillStyle = canvas.fgColor
+		ctx.fillRect(120, bHeight, 560, 80)
+
+		ctx.fillStyle = axis.left.color
+		ctx.fillRect(120, biHeight, 5.6 * resultEffects[axis.id] - 2, 72)
+		ctx.fillStyle = axis.right.color
+		ctx.fillRect(
+			682 - 5.6 * (100 - resultEffects[axis.id]),
+			biHeight,
+			5.6 * (100 - resultEffects[axis.id]) - 2,
+			72
+		)
+
+		ctx.fillStyle = canvas.fgColor
+		ctx.font = "50px " + general.mainFont
+
+		ctx.textAlign = "left"
+		if (resultEffects[axis.id] > 30) {
+			ctx.fillText(resultEffects[axis.id].toFixed(1) + "%", 130, bitHeight)
+		}
+		ctx.textAlign = "right"
+		if (100 - resultEffects[axis.id] > 30) {
+			ctx.fillText(
+				(100 - resultEffects[axis.id]).toFixed(1) + "%",
+				670,
+				bitHeight
 			)
-
-			ctx.fillStyle = canvas.fgColor
-			ctx.font = "50px " + general.mainFont
-
-			ctx.textAlign = "left"
-			if (resultEffects[axis.id] > 30) {
-				ctx.fillText(resultEffects[axis.id].toFixed(1) + "%", 130, bitHeight)
-			}
-			ctx.textAlign = "right"
-			if (100 - resultEffects[axis.id] > 30) {
-				ctx.fillText(
-					(100 - resultEffects[axis.id]).toFixed(1) + "%",
-					670,
-					bitHeight
-				)
-			}
-			ctx.font = "300 30px " + general.mainFont
-			ctx.textAlign = "center"
-			ctx.fillText(axis.name + " Axis: " + matchings[index], 400, biTHeight)
-		})
-	}
+		}
+		ctx.font = "300 30px " + general.mainFont
+		ctx.textAlign = "center"
+		ctx.fillText(axis.name + " Axis: " + matchings[index], 400, biTHeight)
+	})
 }
