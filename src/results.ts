a2200063ec8ba03513facd9cee686531ba9aa153
Vizdun <viz@vizarch.localdomain>
2021-10-06 16:54:02 +0200

    fixed another percantage issue

--- src/results.ts
+++ src/results.ts
@@ -49,14 +49,20 @@ function matchings(resultEffects: { [index: string]: number }) {
 			sum += (100 / presum) * (axes[i]?.weights?.[i2] ?? 1)
 			tiers[i2] = sum
 		}
-
 		for (let i2 = 0; i2 < tiers.length; i2++) {
-			if (
-				100 - resultEffects[axes[i].id] < tiers[i2] ||
-				i2 === tiers.length - 1
-			) {
-				matchings[i] = axes[i].tiers[i2]
-				break
+			if (resultEffects[axes[i].id] > 50) {
+				if (
+					100 - resultEffects[axes[i].id] < tiers[i2] ||
+					i2 === tiers.length - 1
+				) {
+					matchings[i] = axes[i].tiers[i2]
+					break
+				}
+			} else {
+				if (resultEffects[axes[i].id] < tiers[i2] || i2 === tiers.length - 1) {
+					matchings[i] = axes[i].tiers[axes[i].tiers.length - i2]
+					break
+				}
 			}
 		}
 	}
